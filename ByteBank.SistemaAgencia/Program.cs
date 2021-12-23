cbfbbd6fe6f47fafa06b0d843d2f6f27c4c3e2da
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-23 09:23:01 -0300

    adicionando orderBy e expressões lambda

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -2,6 +2,7 @@
 using ByteBank.SistemaAgencia.Comparadores;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -11,18 +12,28 @@ namespace ByteBank.SistemaAgencia
         {
             var contas = new List<ContaCorrente>()
             {
+                null,
                 new ContaCorrente(164, 496979),
                 new ContaCorrente(174, 920050),
                 new ContaCorrente(124, 125668),
                 new ContaCorrente(154, 215896),
-                new ContaCorrente(114, 469709)
+                new ContaCorrente(114, 469709),
+                null
             };
 
-            contas.Sort(new ComparadorContaCorrentePorAgencia());
-
-            foreach (var conta in contas)
+            //contas.Sort(new ComparadorContaCorrentePorAgencia());
+            var contasOrdenadas = contas.OrderBy(conta =>
             {
-                Console.WriteLine($"Número: {conta.Numero} | Agência: {conta.Agencia}");
+                if (conta == null)
+                    return int.MaxValue;
+
+                return conta.Numero;
+            });
+
+            foreach (var conta in contasOrdenadas)
+            {
+                if (conta != null)
+                    Console.WriteLine($"Número: {conta.Numero} | Agência: {conta.Agencia}");
             }
 
             Console.WriteLine("A execução do programa foi finalizada, tecle enter para finalizar ...");
